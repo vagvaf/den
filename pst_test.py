45fb4282a01dbf3af0c12aef5c7c9bf69fd9b090
Evangelos vafeiadis <vagvaf@gmail.com>
2025-02-17 13:09:10 +0100

    Update pst_test.py

--- pst_test.py
+++ pst_test.py
@@ -1,6 +1,8 @@
 import sys, array
+import pandas as pd
+import geopandas
 
-sys.path.append("C:/Users/vagva/OneDrive/Documents/PST_UC/pstqgis_3.3.1_2024-11-01/pst/pstalgo/python")
+sys.path.append("C:/Users/evavaf/OneDrive - Chalmers/Buildingdensities/pst/pstalgo/python")
 import pstalgo
 
 from pstalgo import Radii
@@ -10,7 +12,9 @@ line_coords = array.array('d', [0,0,10,0,10,0,20,0])
 graph = pstalgo.CreateGraph(line_coords, None, None, None, None)  # Or "CreateSegmentGraph" depending on analysis
 
 # Create a radius definition
-radius = Radii(walking = 1000)  
+dist_type='walk'
+dist = 500
+radius = Radii(walking = 500)  
 
 # Count number of lines in the line coordinate array. 
 # There are 2 sets of coordinates for each line, with 
@@ -34,22 +38,101 @@ pstalgo.Reach(
 # Print out the contents of the result array
 print(reached_count)
 
+
 # Free up the mnemory associated with the graph
 pstalgo.FreeGraph(graph)  # Or "FreeSegmentGraph"
 
 
+##################################REACH FOR AN ACTUAL NETWORK####################
+
+roads = geopandas.read_file("C:/Users/evavaf/OneDrive - Chalmers/Buildingdensities/Density types_2017/roads/GOT/got_roads.shp")
+pd.set_option('display.max_colwidth', None)
+
+road_coords = roads.get_coordinates()
+line_coords = []
+for index, row in road_coords.iterrows():
+    line_coords.append(row['x'])
+    line_coords.append(row['y'])
+
+line_coords = array.array('d', line_coords)
+
+line_count = int(len(line_coords) / 4)
+
+unlinks = geopandas.read_file("C:/Users/evavaf/OneDrive - Chalmers/Buildingdensities/Density types_2017/roads/GOT/got_unlinks.shp")
+unlinks_coords = unlinks.get_coordinates()
+point_coords = []
+for index, row in unlinks_coords.iterrows():
+    point_coords.append(row['x'])
+    point_coords.append(row['y'])
+
+point_coords = array.array('d', point_coords)
+
+graph = pstalgo.CreateGraph(line_coords, None, point_coords, None, None)
+
+
+# Prepare 2 arrays for PST to write results into. Size should the number of lines in the network
+reached_count = array.array('I', [0]) * line_count #this is integer
+reached_area = array.array('f', [0]) * line_count #this is integer
+
+# for attraction reach
+#reached_count = array.array('f', [0]) * line_count #this is float
+
+
+# Ask PST to do run a Reach analysis
+pstalgo.Reach(
+    graph_handle = graph,
+    radius = radius,
+    out_reached_count = reached_count,
+    out_reached_area = reached_area)
+
+
+roads[f'RC{dist_type}{dist}']=pd.Series(reached_count)
+roads[f'RA{dist_type}{dist}']=pd.Series(reached_area)
+
+# Free up the mnemory associated with the graph
+pstalgo.FreeGraph(graph)  # Or "FreeSegmentGraph"
+
+print(roads.head(10))
+print(roads.dtypes)
+
+
+
+
+#####################################Attraction reach###########################
+
+buildings = geopandas.read_file("C:/Users/evavaf/OneDrive - Chalmers/Buildingdensities/Density types_2017/roads/GOT/got_buildings.shp")
+
+#calculate the building centroids
+buildings['cnt']=buildings['geometry'].centroid
+
+#get the x and y of the centroid
+buildings['x'] = buildings['cnt'].x
+buildings['y'] = buildings['cnt'].y
+
+b_coords = []
+weight = []
+
+for index, row in buildings.iterrows():
+    b_coords.append(row['x'])
+    b_coords.append(row['y'])
+    weight.append(row['B_Area'])
+
+b_coords = array.array('d', b_coords)
+weight = array.array('f', weight)
+
+print("1")
+
+scores = array.array('f', [0]) * line_count #this is integer
+print("2")
+
+print(len(b_coords),len(weight))
 pstalgo.AttractionReach(
-		graph_handle, #this 
-		origin_type=OriginType.LINES, 
-		distance_type=DistanceType.WALKING, #walking
-		radius=Radii(), #walking 
-		weight_func=AttractionWeightFunction.CONSTANT,
-		weight_func_constant=0,
-		attraction_points=None, #this [0,0,10,10,20,20]
-		points_per_attraction_polygon=None, 
-		attraction_polygon_point_interval=0,
-		attraction_values=None, #weigh by data [2.0,3.0,4.0] 
-		attraction_distribution_func=AttractionDistributionFunc.DIVIDE,
-		attraction_collection_func=AttractionCollectionFunc.AVARAGE,
-		progress_callback = None, 
-		out_scores=None)
+		graph_handle = graph,
+                radius = radius,
+		attraction_points=b_coords,
+		attraction_values=weight,
+		out_scores=scores)
+print("3")
+buildings['score'] = pd.Series(scores)
+
+
